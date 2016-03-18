2cb8c205284e31ead7f5b456308d2f1446580e39
charles peters <charlespeters42@gmail.com>
2016-03-17 22:51:09 -0700

    adding service worker

--- assets/src/js/global.js
+++ assets/src/js/global.js
@@ -2,3 +2,15 @@ window.onload = function () {
   fitVids('.entry');
   loadCSS('//cloud.typography.com/7107912/754766/css/fonts.css');
 };
+
+// ServiceWorker is a progressive technology. Ignore unsupported browsers
+if ('serviceWorker' in navigator) {
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register('/service-worker.js').then(function () {
+    console.log('CLIENT: service worker registration complete.');
+  }, function () {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
