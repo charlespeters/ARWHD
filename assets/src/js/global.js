679df86c3a46e55a692f56f9552279c18254b81e
charles peters <charlespeters42@gmail.com>
2016-03-17 22:56:14 -0700

    cleaning up console.log

--- assets/src/js/global.js
+++ assets/src/js/global.js
@@ -8,9 +8,11 @@ if ('serviceWorker' in navigator) {
   console.log('CLIENT: service worker registration in progress.');
   navigator.serviceWorker.register('/service-worker.js').then(function () {
     console.log('CLIENT: service worker registration complete.');
-  }, function () {
+  }, ()=> {
     console.log('CLIENT: service worker registration failure.');
   });
 } else {
   console.log('CLIENT: service worker is not supported.');
-}
+};
+
+console.log('stuff is happening');
