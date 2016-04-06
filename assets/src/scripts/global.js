c4f44a41d6cfc5bc8c941a2b82cf433800393324
charles peters <charlespeters42@gmail.com>
2016-04-06 13:19:40 -0700

    analytics in separate script

--- assets/src/scripts/global.js
+++ assets/src/scripts/global.js
@@ -7,7 +7,6 @@ var fonts = '//cloud.typography.com/7107912/754766/css/fonts.css';
 var arwhd = window.arwhd || {};
 
 arwhd.Worker = require('./lib/sw');
-arwhd.Tracking = require('./lib/tracking');
 arwhd.CSS = stylesheet(fonts);
 arwhd.Highlighting = prism;
 arwhd.Fit = fitvids('._s-entry');
