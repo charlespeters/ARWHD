093be884451ddf21ecba441ec156b3c08f315107
charles peters <charlespeters42@gmail.com>
2016-05-21 21:42:18 -0700

    gulp 4 babel stuff

--- Gulpfile.babel.js
+++ Gulpfile.babel.js
@@ -1,152 +1,178 @@
 import gulp from 'gulp';
-var gutil       = require('gulp-util');
-var plumber     = require('gulp-plumber');
-var browserSync = require('browser-sync');
-var reload      = browserSync.reload();
-var postcss     = require('gulp-postcss');
-var browserify  = require('browserify');
-var babelify    = require('babelify');
-var watchify    = require('watchify');
-var source      = require('vinyl-source-stream');
-var buffer      = require('vinyl-buffer');
-var uglify      = require('gulp-uglify');
-var imagemin    = require('gulp-imagemin');
-var cheerio     = require('gulp-cheerio');
-var svgmin      = require('gulp-svgmin');
-var nano        = require('gulp-cssnano');
-var uncss       = require('gulp-uncss');
-var svgstore    = require('gulp-svgstore');
-var size        = require('gulp-size');
-var concat      = require('gulp-concat');
-var psi         = require('psi');
-var cp          = require('child_process');
-var paths       = require('./paths');
-var name        = 'arwhd';
+import del from 'del';
+import rename from 'gulp-rename';
+import plumber from 'gulp-plumber';
+import size from 'gulp-size';
+import svgmin from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import cheerio from 'gulp-cheerio';
+import postcss from 'gulp-postcss';
+import cssnext from 'postcss-cssnext';
+import atImport from 'postcss-import';
+import papply from 'postcss-apply';
+import reporter from 'postcss-reporter';
+import nano from 'cssnano';
+import stylelint from 'stylelint';
+import eslint from 'gulp-eslint';
+import imagemin from 'gulp-imagemin';
+import uglify from 'gulp-uglify';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import watchify from 'watchify';
+import source from 'vinyl-source-stream';
+import buffer from 'vinyl-buffer';
+import browserSync from 'browser-sync';
+import paths from './paths';
 
-const cssnext    = require('postcss-cssnext');
-const atImport   = require('postcss-import');
-
-const processors = [
-  atImport,
-  cssnext({
-    browsers: ['last 2 version'],
-    features: {
-      colorFunction: true,
-      customSelectors: true,
-      rem: false,
-    },
-  }),
-];
-
-var messages = {
-  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build',
-};
-
-// Jekyll Building
-
-gulp.task('jekyll-build', function (done) {
-  browserSync.notify(messages.jekyllBuild);
-  return cp.spawn('jekyll', ['build', '--drafts'], { stdio: 'inherit' }).on('close', done);
-});
-
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-  browserSync.reload();
-});
+const cp = require('child_process');
+const bs = browserSync.create();
 
 // Asset Building
 
-gulp.task('styles', () => {
-  return gulp.src(paths.cssI)
-  .pipe(postcss(processors))
-  .pipe(uncss({
-    html: ['./_site/**/*.html'],
-  }))
-  .pipe(nano({ mergeRules: false }))
-  .pipe(size({
-    showFiles: true,
-    gzip: true,
-  }))
-  .pipe(gulp.dest('_site/' + paths.dist + 'css/'))
-  .pipe(browserSync.reload({ stream: true }))
-  .pipe(gulp.dest(paths.dist + 'styles/'))
-  .pipe(gulp.dest('_includes/'));
-});
 
-// Transpile JavaScript through Browserify with Babel
-const bundler = watchify(browserify('./assets/src/scripts/global.js', watchify.args));
+// Styles
+/////////////////////////
+
+const styles = () => {
+  const processors = [
+    atImport,
+    cssnext({
+      warnForDuplicates: false,
+      browsers: ['last 2 version'],
+      features: {
+        colorFunction: true,
+        customSelectors: true,
+        rem: false
+      },
+    }),
+    papply,
+    nano({ mergeRules: false })
+  ];
+
+  return gulp.src(paths.css.src)
+    .pipe(plumber())
+    .pipe(postcss(processors))
+    .pipe(size({
+      showFiles: true,
+      gzip: true,
+    }))
+    .pipe(gulp.dest(paths.css.dest))
+    .pipe(gulp.dest('./_site/assets/dist/styles/'))
+    .pipe(gulp.dest('./_includes/'))
+    .pipe(bs.stream());
+};
+
+// Scripts
+/////////////////////////
+
+const bundler = watchify(browserify(paths.js.src, watchify.args));
 
 function bundle() {
   return bundler
     .transform(babelify)
     .bundle()
-    .on('error', gutil.log)
     .pipe(source('./bundle.js'))
       .pipe(buffer())
-      .on('error', gutil.log)
       .pipe(uglify())
-      .on('error', gutil.log)
-    .pipe(gulp.dest('./assets/dist/scripts/'))
-    .pipe(browserSync.reload({ stream: true }));
-};
+      .pipe(size({
+        showFiles: true,
+        gzip: true,
+      }))
+    .pipe(gulp.dest(paths.js.dest))
+    .pipe(gulp.dest('./_site/assets/dist/scripts/'))
+    .pipe(bs.stream());
+}
 
 bundler.on('update', bundle);
-gulp.task('scripts', bundle);
 
-gulp.task('images', function () {
-  return gulp.src(paths.img)
-  .pipe(size())
-  .pipe(imagemin({
-    progressive: true,
-    svgoPlugins: [{ removeViewBox: false }],
-  }))
-  .pipe(gulp.dest('_site/' + paths.dist + 'img/'))
-  .pipe(gulp.dest(paths.dist + 'img/'));
-});
+const scripts = bundle;
 
-gulp.task('icons', function () {
-  return gulp.src(paths.icons)
+// Image Processing
+/////////////////////////
+
+const images = () => {
+  return gulp.src(paths.img.src)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{ removeViewBox: false }],
+    }))
+    .pipe(size({
+      showFiles: true,
+      gzip: true,
+    }))
+    .pipe(gulp.dest(paths.img.dest));
+};
+
+// Icons as SVG Sprite
+/////////////////////////
+
+const icons = () => {
+  return gulp.src(paths.icons.src)
     .pipe(svgmin())
-    .pipe(svgstore({ fileName: 'icons.svg', inlineSvg: true }))
+    .pipe(svgstore({
+      fileName: 'icons.svg',
+      inlineSvg: true
+    }))
     .pipe(cheerio({
-      run: function ($, file) {
-        $('svg').addClass('u-d-none');
+      run: ($, file) => {
+        $('svg').addClass('u-none');
         $('[fill]').removeAttr('fill');
       },
-
       parserOptions: { xmlMode: true },
     }))
-    .pipe(gulp.dest('./_includes/'));
+    .pipe(gulp.dest(paths.icons.dest))
+    .pipe(bs.stream());
+};
+
+// Jekyll
+/////////////////////////
+
+const messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build',
+};
+
+const jekyll = (done) => {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn('jekyll', ['build', '--drafts'], { stdio: 'inherit' }).on('close', done);
+};
+
+const rejekyll = gulp.series(jekyll, () => {
+  return bs.stream();
 });
 
-gulp.task('details', function () {
-  var site = 'http://arwhd.co/';
-  return psi(site, {
-      nokey: 'true',
-      strategy: 'mobile',
-    }).then(function (data) {
-      console.log('Speed score: ' + data.ruleGroups.SPEED.score);
-      console.log('Usability score: ' + data.ruleGroups.USABILITY.score);
-    });
+// Clean Build Directory
+/////////////////////////
+
+const clean = () => del(paths.build);
+
+// Server
+/////////////////////////
+
+const connect = () => bs.init({
+  server: {
+    baseDir: paths.build,
+  },
 });
 
-// Connect & Deploy
-gulp.task('connect', ['styles', 'jekyll-build'], function () {
-  browserSync({
-    server: {
-      baseDir: '_site',
-      https: true,
-    },
-  });
-});
+// Watch
+/////////////////////////
 
-gulp.task('watch', function () {
-  gulp.watch([paths.cssI, paths.cssDir], ['styles', 'jekyll-rebuild']);
-  gulp.watch(paths.js.src, ['scripts', 'jekyll-rebuild']);
-  gulp.watch(paths.icons, ['icons', 'jekyll-rebuild']);
-  gulp.watch(paths.img, ['images', 'jekyll-rebuild']);
-  gulp.watch(paths.markup, ['jekyll-rebuild']);
-});
+const watch = () => {
+  gulp.watch(paths.css.all, gulp.series(styles, rejekyll));
+  gulp.watch(paths.js.all, gulp.series(scripts, rejekyll));
+  gulp.watch(paths.markup, rejekyll);
+  gulp.watch(paths.icons.src, gulp.series(icons, rejekyll));
+  gulp.watch(paths.img.src, gulp.series(images, rejekyll));
+};
 
-// Task Registry
-gulp.task('build', ['styles', 'scripts', 'images']);
-gulp.task('default', ['build', 'jekyll-rebuild', 'connect', 'watch']);
+// Exports Functions as Proper Tasks
+
+export { clean, styles, scripts, icons, images, watch, connect, jekyll, rejekyll };
+
+// Default Tasks
+/////////////////////////
+
+const build = gulp.series(clean, gulp.parallel(styles, scripts, rejekyll, images));
+const all = gulp.series(build, gulp.parallel(connect, watch));
+
+export default all;
--- paths.js
+++ paths.js
@@ -1,15 +1,24 @@
 var paths = {
   cssI: 'assets/src/styles/arwhd.css',
   cssDir: 'assets/src/styles/**/*.css',
-  js: {
-    inp: './assets/src/scripts/global.js',
-    src: './assets/src/scripts/**/*.js',
-    dist: './assets/dist/scripts/',
+  css: {
+    src: './assets/src/styles/arwhd.css',
+    all: './assets/src/styles/**/*.css',
+    dest: './assets/dist/styles/'
   },
-  icons: 'assets/src/icons/*.svg',
+  js: {
+    src: './assets/src/scripts/global.js',
+    all: './assets/src/scripts/**/*.js',
+    dest: './assets/dist/scripts/',
+  },
+  icons: {
+    src: './assets/src/icons/*.svg',
+    dest: './_includes/'
+  },
   img: 'assets/src/img/*',
   markup: ['./*.html', './*.md', '_includes/*.html', '_layouts/*.html', '_posts/*', '_drafts/*'],
   dist: './assets/dist/',
+  build: './_site/'
 };
 
 module.exports = paths;
