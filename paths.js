2ef511dd505f500737b85e8f3c152f91194ad890
charles peters <charlespeters42@gmail.com>
2016-05-27 01:19:25 -0700

    cleaning up asset directories

--- paths.js
+++ paths.js
@@ -1,25 +1,23 @@
 var paths = {
-  cssI: 'assets/src/styles/arwhd.css',
-  cssDir: 'assets/src/styles/**/*.css',
   css: {
-    src: './assets/src/styles/arwhd.css',
-    all: './assets/src/styles/**/*.css',
+    src: './_assets/styles/arwhd.css',
+    all: './_assets/styles/**/*.css',
     dest: './assets/dist/styles/'
   },
   js: {
-    src: './assets/src/scripts/global.js',
-    all: './assets/src/scripts/**/*.js',
+    src: './_assets/scripts/global.js',
+    all: './_assets/scripts/**/*.js',
     dest: './assets/dist/scripts/',
   },
   icons: {
-    src: './assets/src/icons/*.svg',
+    src: './_assets/icons/*.svg',
     dest: './_includes/'
   },
   img: {
-    src: './assets/src/img/*',
+    src: './_assets/img/*',
     dest: './assets/dist/img/'
   },
-  markup: ['./*.html', './*.md', '_includes/*.html', '_layouts/*.html', '_posts/*', '_drafts/*'],
+  markup: ['./*.html', './*.md', './_includes/**/*', './_layouts/*.html', './_posts/*', './_drafts/*'],
   dist: './assets/dist/',
   build: './_site/'
 };
